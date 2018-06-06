9e0fb2b448578a0386ec2fad45af4f7e43054e87
Mike Belhomme <34034247+Azoth38@users.noreply.github.com>
2018-06-06 22:39:32 +0200

    Détection des cliques
    Détection des cliques sur les pays reste a relier a ton code en changeant les varialbes

--- src/Risk/Interface.java
+++ src/Risk/Interface.java
@@ -150,32 +150,161 @@ public class Interface extends JFrame implements ActionListener {
 
 
 	class Panneau extends JPanel{
+		public Panneau() {
+			 this.addMouseListener(this);    
+		}
+		//Europe
+		
+		int x2=464;
+		int y2=105;
+		int x5=530;
+		int y5=240;
+		int x1=413;
+		int y1=173;
+		int x6=595;
+		int y6=165;
+		int x7=455;
+		int y7=235;
+		int x3=520;
+		int y3=185;
+		int x4=525;
+		int y4=100;
+		Ellipse2D oval1 = new Ellipse2D.Double(x1, y1, 20, 20);
+		Ellipse2D oval2 = new Ellipse2D.Double(x2, y2, 17, 17);
+		Ellipse2D oval3 = new Ellipse2D.Double(x3, y3, 20, 20);
+		Ellipse2D oval4 = new Ellipse2D.Double(x4, y4, 20, 20);
+		Ellipse2D oval5 = new Ellipse2D.Double(x5, y5, 20, 20);
+		Ellipse2D oval6 = new Ellipse2D.Double(x6, y6, 20, 20);
+		Ellipse2D oval7 = new Ellipse2D.Double(x7, y7, 20, 20);
+		//Asie
+		int x12=998;
+		int y12=200;
+		int x15=900;
+		int y15=190;
+		int x11=870;
+		int y11=135;
+		int x16=875;
+		int y16=330;
+		int x17=800;
+		int y17=100;
+		int x18=730;
+		int y18=120;
+		int x13=950;
+		int y13=60;
+		int x14=650;
+		int y14=300;	
+		int x10=780;
+		int y10=310;
+		int x8=710;
+		int y8=200;
+		int x9=840;
+		int y9=250;
+		int x19=885;
+		int y19=50;
+		
+		
+		Ellipse2D oval8 = new Ellipse2D.Double(x8, y8, 20, 20);
+		Ellipse2D oval9 = new Ellipse2D.Double(x9, y9, 20, 20);
+		Ellipse2D oval10 = new Ellipse2D.Double(x10, y10, 20, 20);
+		Ellipse2D oval11 = new Ellipse2D.Double(x11, y11, 20, 20);
+		Ellipse2D oval12 = new Ellipse2D.Double(x12, y12, 20, 20);
+		Ellipse2D oval13 = new Ellipse2D.Double(x13, y13, 20, 20);
+		Ellipse2D oval14 = new Ellipse2D.Double(x14, y14, 20, 20);
+		Ellipse2D oval15 = new Ellipse2D.Double(x15, y15, 20, 20);
+		Ellipse2D oval16 = new Ellipse2D.Double(x16, y16, 20, 20);
+		Ellipse2D oval17 = new Ellipse2D.Double(x17, y17, 20, 20);
+		Ellipse2D oval18 = new Ellipse2D.Double(x18, y18, 20, 20);
+		Ellipse2D oval19 = new Ellipse2D.Double(x19, y19, 20, 20);
+		//NA
+		
+		int x20=65;
+		int y20=80;
+		int x25=170;
+		int y25=81;
+		int x21=150;
+		int y21=120;
+		int x26=197;
+		int y26=125;
+		int x27=285;
+		int y27=123;
+		int x28=145;
+		int y28=190;
+		int x23=230;
+		int y23=195;
+		int x22=152;
+		int y22=270;	
+		int x24=360;
+		int y24=50;
+		Ellipse2D oval20 = new Ellipse2D.Double(x20, y20, 20, 20);
+		Ellipse2D oval21 = new Ellipse2D.Double(x21, y21, 20, 20);
+		Ellipse2D oval22 = new Ellipse2D.Double(x22, y22, 20, 20);
+		Ellipse2D oval23 = new Ellipse2D.Double(x23, y23, 20, 20);
+		Ellipse2D oval24 = new Ellipse2D.Double(x24, y24, 20, 20);
+		Ellipse2D oval25 = new Ellipse2D.Double(x25, y25, 20, 20);
+		Ellipse2D oval26 = new Ellipse2D.Double(x26, y26, 20, 20);
+		Ellipse2D oval27 = new Ellipse2D.Double(x27, y27, 20, 20);
+		Ellipse2D oval28 = new Ellipse2D.Double(x28, y28, 20, 20);
+	
+	
+		//SA
+		int x30=320;
+		int y30=390;
+		int x29=255;
+		int y29=487;
+		int x31=225;
+		int y31=405;
+		int x32=223;
+		int y32=322;
+		Ellipse2D oval29 = new Ellipse2D.Double(x29, y29, 20, 20);
+		Ellipse2D oval30 = new Ellipse2D.Double(x30, y30, 20, 20);
+		Ellipse2D oval31 = new Ellipse2D.Double(x31, y31, 20, 20);
+		Ellipse2D oval32 = new Ellipse2D.Double(x32, y32, 20, 20);
+		//Afrique
+		
+		int x37=468;
+		int y37=350;
+		int x35=563;
+		int y35=340;
+		int x34=620;
+		int y34=400;
+		int x33=570;
+		int y33=440;
+		int x38=580;
+		int y38=515;
+		int x36=680;
+		int y36=525;
+		Ellipse2D oval33 = new Ellipse2D.Double(x33, y33, 20, 20);
+		Ellipse2D oval34 = new Ellipse2D.Double(x34, y34, 20, 20);
+		Ellipse2D oval35 = new Ellipse2D.Double(x35, y35, 20, 20);
+		Ellipse2D oval36 = new Ellipse2D.Double(x36, y36, 20, 20);
+		Ellipse2D oval37 = new Ellipse2D.Double(x37, y37, 20, 20);
+		Ellipse2D oval38 = new Ellipse2D.Double(x38, y38, 20, 20);
+		   //Oceanie 
+		int x40=890;
+		int y40=430;
+		int x41=980;
+		int y41=410;
+		int x42=940;
+		int y42=520;
+		int x39=1020;
+		int y39=500;
+		Ellipse2D oval39 = new Ellipse2D.Double(x39, y39, 20, 20);
+		Ellipse2D oval40 = new Ellipse2D.Double(x40, y40, 20, 20);
+		Ellipse2D oval41 = new Ellipse2D.Double(x41, y41, 20, 20);
+		Ellipse2D oval42 = new Ellipse2D.Double(x42, y42, 20, 20);
+	
 		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
+			
 			try {
-
-
+				Graphics2D g2d = (Graphics2D) g ;
+				g2d.draw(oval1);
+				
 				Image img = ImageIO.read(new File("Risk'isep_country_id.png"));
-				g.drawImage(img, 0, 0,1280 ,600,this);
-					//NA
-				int x20=65;
-				int y20=80;
-				int x25=170;
-				int y25=81;
-				int x21=150;
-				int y21=120;
-				int x26=197;
-				int y26=125;
-				int x27=285;
-				int y27=123;
-				int x28=145;
-				int y28=190;
-				int x23=230;
-				int y23=195;
-				int x22=152;
-				int y22=270;	
-				int x24=360;
-				int y24=50;
+				g2d.drawImage(img, 0, 0,1280 ,600,this);
+				
+				
+				
+				//NA
 				g.fillOval(x20,y20,20,20);
 				g.fillOval(x25,y25,20,20);
 				g.fillOval(x21,y21,20,20);
@@ -186,34 +315,13 @@ public class Interface extends JFrame implements ActionListener {
 				g.fillOval(x21,y21,20,20);
 				g.fillOval(x22,y22,20,20);
 				g.fillOval(x24,y24,20,20);
+
 				//SA
-				int x30=320;
-				int y30=390;
-				int x29=255;
-				int y29=487;
-				int x31=225;
-				int y31=405;
-				int x32=223;
-				int y32=322;
 				g.fillOval(x30,y30,20,20);
 				g.fillOval(x29,y29,20,20);
 				g.fillOval(x31,y31,20,20);
 				g.fillOval(x32,y32,20,20);
 				//Europe
-				int x2=464;
-				int y2=105;
-				int x5=530;
-				int y5=240;
-				int x1=413;
-				int y1=173;
-				int x6=595;
-				int y6=165;
-				int x7=455;
-				int y7=235;
-				int x3=520;
-				int y3=185;
-				int x4=525;
-				int y4=100;
 				g.fillOval(x2,y2,17,17);
 				g.fillOval(x5,y5,20,20);
 				g.fillOval(x1,y1,20,20);
@@ -222,19 +330,6 @@ public class Interface extends JFrame implements ActionListener {
 				g.fillOval(x4,y4,20,20);
 				g.fillOval(x3,y3,20,20);
 				//Afrique
-				
-				int x37=468;
-				int y37=350;
-				int x35=563;
-				int y35=340;
-				int x34=620;
-				int y34=400;
-				int x33=570;
-				int y33=440;
-				int x38=580;
-				int y38=515;
-				int x36=680;
-				int y36=525;
 				g.fillOval(x37,y37,20,20);
 				g.fillOval(x35,y35,20,20);
 				g.fillOval(x34,y34,20,20);
@@ -242,30 +337,6 @@ public class Interface extends JFrame implements ActionListener {
 				g.fillOval(x38,y38,20,20);
 				g.fillOval(x36,y36,17,17);
 				//Asie
-				int x12=998;
-				int y12=200;
-				int x15=900;
-				int y15=190;
-				int x11=870;
-				int y11=135;
-				int x16=875;
-				int y16=330;
-				int x17=800;
-				int y17=100;
-				int x18=730;
-				int y18=120;
-				int x13=950;
-				int y13=60;
-				int x14=650;
-				int y14=300;	
-				int x10=780;
-				int y10=310;
-				int x8=710;
-				int y8=200;
-				int x9=840;
-				int y9=250;
-				int x19=885;
-				int y19=50;
 				g.fillOval(x12,y12,20,20);
 				g.fillOval(x15,y15,20,20);
 				g.fillOval(x11,y11,20,20);
@@ -278,23 +349,215 @@ public class Interface extends JFrame implements ActionListener {
 				g.fillOval(x8,y8,20,20);
 				g.fillOval(x9,y9,20,20);
 				g.fillOval(x19,y19,20,20);
-			    //Oceanie 
-				int x40=890;
-				int y40=430;
-				int x41=980;
-				int y41=410;
-				int x42=940;
-				int y42=520;
-				int x39=1020;
-				int y39=500;
+				//Oceanie 
 				g.fillOval(x40,y40,20,20);
 				g.fillOval(x41,y41,20,20);
 				g.fillOval(x42,y42,20,20);
 				g.fillOval(x39,y39,20,20);
+				
 			}catch(IOException e) {
 				e.printStackTrace();
 			}
 
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if (oval1.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui1");
+	        }
+			if (oval2.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui2");
+	        }
+			
+			if (oval3.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui3");
+	        }
+			
+			if (oval4.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui4");
+	        }
+			
+			if (oval5.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui5");
+	        }
+			
+			if (oval6.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui6");
+	        }
+			
+			if (oval7.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui7");
+	        }
+			
+			if (oval8.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui8");
+	        }
+			
+			if (oval9.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui9");
+	        }
+			
+			if (oval10.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui10");
+	        }
+			
+			if (oval11.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui11");
+	        }
+			
+			if (oval12.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui12");
+	        }
+			
+			if (oval13.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui13");
+	        }
+			
+			if (oval14.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui14");
+	        }
+			
+			if (oval15.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui15");
+	        }
+			
+			if (oval16.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui16");
+	        }
+			
+			if (oval17.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui17");
+	        }
+			
+			if (oval18.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui18");
+	        }
+			
+			if (oval18.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui18");
+	        }
+			
+			if (oval19.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui19");
+	        }
+			
+			if (oval20.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui20");
+	        }
+			
+			if (oval21.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui21");
+	        }
+			
+			if (oval22.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui22");
+	        }
+			
+			if (oval23.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui23");
+	        }
+			
+			if (oval24.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui24");
+	        }
+			
+			if (oval25.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui25");
+	        }
+			
+			if (oval26.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui26");
+	        }
+			
+			if (oval27.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui27");
+	        }
+			
+			if (oval28.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui28");
+	        }
+			
+			if (oval29.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui29");
+	        }
+			
+			if (oval30.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui30");
+	        }
+			
+			if (oval31.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui31");
+	        }
+			
+			if (oval32.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui32");
+	        }
+			
+			if (oval33.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui33");
+	        }
+			
+			if (oval34.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui34");
+			}
+			
+			if (oval35.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui35");
+			}
+			
+			if (oval36.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui36");
+			}
+			
+			if (oval37.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui37");
+			}
+			
+			if (oval38.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui38");
+			}
+
+			if (oval39.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui39");
+			}
+			
+			if (oval40.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui40");
+			}
+			
+			
+			if (oval41.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui41");
+			}
+			
+			
+			if (oval42.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Oui42");
+			}
+			
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if (oval1.contains(e.getX(), e.getY()) ) {
+	            System.out.println("Non");
+	        }
+		}
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
 		}		 
 	}
 	class Panneau1 extends JPanel{
